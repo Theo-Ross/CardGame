6cee779ecff12eb301c8f819b8dd0fb88c2b5523
Theo-Ross <TheoRoss99@icloud.com>
2023-01-10 14:31:09 +0000

    start

---
+++ src/main/java/org/example/Cards.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Cards {
+    private String suit;
+    private String symbol;
+    private int value;
+
+    public Cards(String suit, String symbol, int value) {
+        this.suit = suit;
+        this.symbol = symbol;
+        this.value = value;
+    }
+}
