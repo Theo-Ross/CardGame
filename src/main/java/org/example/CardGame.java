e30c8a2bba837ce9751304ad15f7dd0cee7d517b
Theo-Ross <TheoRoss99@icloud.com>
2023-01-11 17:51:15 +0000

    snap finished

---
+++ src/main/java/org/example/CardGame.java
@@ -0,0 +1,52 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CardGame {
+    private static ArrayList<Card> deckOfCards = new ArrayList<>(52);
+
+    public static ArrayList<Card> player1Cards = new ArrayList<>(52);
+
+    public static ArrayList<Card> player2Cards = new ArrayList<>(52);
+
+
+
+
+    public void setup(){
+
+        String[] suits = {"♥","♣","♦","♠"};
+        String[] symbols = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+
+        for (int i = 0; i < suits.length; i++) {
+            for (int j = 0; j < symbols.length; j++) {
+                deckOfCards.add(new Card(suits[i], symbols[j], j + 1));
+            }
+        }
+
+
+    }
+
+
+
+    public static void dealCards() {
+        Collections.shuffle(deckOfCards);
+        for (int i = 0; i < deckOfCards.size(); i++) {
+            if (i % 2 == 0) {
+                player1Cards.add(deckOfCards.get(i));
+            } else {
+                player2Cards.add(deckOfCards.get(i));
+            }
+
+        }
+    }
+
+
+
+
+
+
+
+
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,42 @@
 package org.example;
 
+
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public static void main(String[] args) {
+    CardGame cardGame = new CardGame();
+    Snap snap = new Snap();
+
+    boolean playing = true;
+
+    Scanner scanner = new Scanner(System.in);
+
+    while (playing) {
+        System.out.println("Welcome to Hangman press Enter to play");
+        cardGame.setup();
+        cardGame.dealCards();
+        snap.play();
+
+        System.out.println("Would you like to play again? Y/N");
+        if (scanner.nextLine().equals("N")) {
+            playing = false;
+        }
+
+
+        System.out.println("Ok, thank you for playing");
+
+
     }
+
+
+}
+
+
+
+
+
+
+
+
 }
